6e6ff00f1a769378e2d1672af201d62e6251bd64
Piyush Keshri <piyushkeshri383@gmail.com>
2022-01-19 17:19:48 +0530

    commit

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo  = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+const nav = document.querySelector('.nav');
+
+
 //-----------------------MODAL WINDOW------------------------------//
 const openModal = function (e) {
   e.preventDefault();      
@@ -91,22 +98,61 @@ btnScrollTo.addEventListener('click',function(e){
 
 //------------------------------TABBED COMPONENT----------------------------------//
 
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
-
 tabsContainer.addEventListener('click',function(e){
 
 const clicked  = e.target.closest('.operations__tab');
-console.log('clicked');
 
 //Gaurd Clause
 if(!clicked){ 
 return;
 }
-
+//Active tab 
 tabs.forEach(t => t.classList.remove('operations__tab--active'));
 
 clicked.classList.add('operations__tab--active');
 
-})
+//Activate tab
+tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+//Activate content area.
+document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
+
+});
+
+//-----------------------Menu Fade Animation------------------------------//
+const handleHover = function(e){
+
+if(e.target.classList.contains('nav__link')){
+
+const link = e.target;
+const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+const logo = link.closest('.nav').querySelector('img');
+
+siblings.forEach(el => {
+
+  if(el !== link) el.style.opacity = this;
+
+});
+logo.style.opacity = this;
+
+}
+}
+
+nav.addEventListener('mouseover',handleHover.bind(0.5));
+nav.addEventListener('mouseout',handleHover.bind(1));
+
+//------------------------STICKY NAVIGATION--------------------------//
+
+const initialCoords = section1.getBoundingClientRect();
+console.log(initialCoords);
+
+window.addEventListener('scroll',function(){
+// console.log(window.scrollY)
+if(window.scrollY > initialCoords.top)
+nav.classList.add('sticky');
+
+else
+nav.classList.remove('sticky');
+
+});
+
